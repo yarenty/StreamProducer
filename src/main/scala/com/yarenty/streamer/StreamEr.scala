0439007377b9ad36a1779ae0d11f2f0700feea76
yarenty <yarenty@gmail.com>
2018-08-13 15:33:59 +0100

    all together

--- src/main/scala/com/yarenty/streamer/StreamEr.scala
+++ src/main/scala/com/yarenty/streamer/StreamEr.scala
@@ -3,16 +3,27 @@ package com.yarenty.streamer
 
 /**
   * Stream-Er - process input CSV file and send stream of data to specific URL with configured interval.
-  * 
+  *
   * (C)2018 by yarenty
   */
-object StreamEr  {
-  
- def main(args: Array[String]) {
+object StreamEr {
 
-   implicit val config = new Config(args)
+  def main(args: Array[String]) {
+
+    implicit val config = new Config(args)
+
+    println("Stream-Er") // scalastyle:ignore
+
+    val input: CSVProcessor = new CSVToHttpGetProcessor
+    val sender: HttpSender = new HttpGetSender
+
+
+    while (input.hasMore) {
+      wait(5000)
+      sender.send(input.getLine())
+    }
+
+    println("That's all folks!!") //scalastyle:ignore
    
-   println("Stream-Er") // scalastyle:ignore
-
- }  
+  }
 }
